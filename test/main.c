053dc4cbbcf6f3e689cbbbb27ade34a5e288256c
Ole <itstheimp@gmail.com>
2014-05-29 00:59:11 +0200

    test: trying the new features

--- test/main.c
+++ test/main.c
@@ -1,7 +1,21 @@
+#include "../src/tokenizer.h"
+#include "../src/chunklist.h"
+
 #include <stdio.h>
+#include <malloc.h>
+#include <string.h>
 
 int main(void) {
-	printf("Hi!\n");
+	input_t in = input_new("test/example.olec");
+
+	token_t tok;
+
+	while ((tok = input_identifier(in)) != NULL) {
+		printf("[%lu, %lu]: %s\n", tok->line, tok->column, tok->contents);
+		token_free(tok);
+	}
+
+	input_free(in);
 
 	return 0;
 }
