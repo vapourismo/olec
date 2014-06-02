5a19eda3ac2950a74566a95e57be1692aaf37110
Ole <itstheimp@gmail.com>
2014-06-02 15:43:02 +0200

    example

--- test/main.c
+++ test/main.c
@@ -10,9 +10,17 @@ int main(void) {
 
 	token_t tok;
 
-	while ((tok = input_identifier(in)) != NULL) {
-		printf("[%lu, %lu]: %s\n", tok->line, tok->column, tok->contents);
+	while ((tok = input_tokenize(in)) != NULL) {
+		printf("%s [%lu, %lu]: %s\n", token_type_name(tok->type), tok->line, tok->column, tok->contents);
 		token_free(tok);
+	}
+
+	size_t ln, col;
+
+	if (input_done(in, &ln, &col)) {
+		printf("> Done\n");
+	} else {
+		printf("> Failed (%lu, %lu)\n", ln, col);
 	}
 
 	input_free(in);
