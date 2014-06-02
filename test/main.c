23357e0edea732031eac6a44a6d33ad01aa20942
Ole <itstheimp@gmail.com>
2014-06-02 19:24:14 +0200

    example

--- test/main.c
+++ test/main.c
@@ -9,15 +9,15 @@ int main(void) {
 	input_t* in = input_new("test/example.olec");
 	token_t* tok;
 
-	while ((tok = input_tokenize(in)) != NULL) {
-		printf("%s [%lu, %lu]: %s\n", token_type_name(tok->type), tok->line, tok->column, tok->contents);
+	while ((tok = input_tokenize(in)) != NULL && tok->type != T_ERROR) {
+		printf("%s [%lu, %lu]: %s\n", token_type_name(tok->type),
+		       tok->line, tok->column,
+		       tok->data.token);
 		token_free(tok);
 	}
 
-	if (input_done(in)) {
-		printf("> Done\n");
-	} else {
-		printf("> Failed (%lu, %lu)\n", in->line, in->column);
+	if (tok && tok->type == T_ERROR) {
+		printf("> Failed (%lu, %lu): %s\n", tok->line, tok->column, tok->data.error.message);
 	}
 
 	input_free(in);
