e65b458e1e8881bc42b7afa15d01d69a3b538f4e
Ole <itstheimp@gmail.com>
2014-06-03 22:54:18 +0200

    tokenizer: tokpatterns are set

--- lib/tokenizer.c
+++ lib/tokenizer.c
@@ -1,2 +1,39 @@
 #include "tokenizer.h"
+#include "aux.h"
 
+tokpattern_t* tokpattern_new(size_t id, const char* preg) {
+	tokpattern_t* tp = new(tokpattern_t);
+
+	if (tp) {
+		/* compile the regular expression */
+		if (regcomp(&tp->pattern, preg, REG_EXTENDED) != 0) {
+			free(tp);
+			return NULL;
+		}
+
+		tp->id = id;
+	}
+
+	return tp;
+}
+
+void tokpattern_free(tokpattern_t* tp) {
+	if (tp) {
+		/* the regex needs to be freed seperately */
+		regfree(&tp->pattern);
+
+		free(tp);
+	}
+}
+
+ssize_t tokpattern_check(const tokpattern_t* tp, const char* input) {
+	regmatch_t m;
+
+	if (regexec(&tp->pattern, input, 1, &m, 0) == 0) {
+		/* since we do not want to skip input,
+		   we have to make sure the match is at offset 0 */
+		return m.rm_so == 0 ? m.rm_eo : -1;
+	} else {
+		return -1;
+	}
+}
--- lib/tokenizer.h
+++ lib/tokenizer.h
@@ -1,6 +1,27 @@
 #ifndef OLEC_TOKENIZER_H
 #define OLEC_TOKENIZER_H
 
+#include <unistd.h>
+#include <regex.h>
 
+typedef struct {
+	size_t id;
+	regex_t pattern;
+} tokpattern_t;
+
+/**
+ * Create a new token pattern.
+ */
+tokpattern_t* tokpattern_new(size_t id, const char* preg);
+
+/**
+ * Free a token pattern.
+ */
+void tokpattern_free(tokpattern_t* tp);
+
+/**
+ * Check whether the token pattern applies to the given input.
+ */
+ssize_t tokpattern_check(const tokpattern_t* tp, const char* input);
 
 #endif
