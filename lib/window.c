916075128eb1b86dc968fcf8540a67d5f570a55f
Ole <itstheimp@gmail.com>
2014-06-04 23:38:01 +0200

    window: revamped interface

--- lib/window.c
+++ lib/window.c
@@ -1,27 +1,24 @@
 #include "window.h"
 #include "aux.h"
+#include <string.h>
 
-void window_create(window_t* win,
-                   size_t x, size_t y, size_t w, size_t h) {
-	win->ref = newwin(h, w, y, x);
-	win->parent = NULL;
+window_t* window_new(size_t x, size_t y, size_t w, size_t h) {
+	return newwin(h, w, y, x);
 }
 
-void window_delete(const window_t* win) {
-	if (win->ref)
-		delwin(win->ref);
+void window_free(window_t* win) {
+	delwin(win);
 }
 
 void window_get_bounds(const window_t* win, rect_t* bounds) {
-	bounds->x = getbegx(win->ref);
-	bounds->y = getbegy(win->ref);
-	bounds->w = getmaxx(win->ref);
-	bounds->h = getmaxy(win->ref);
+	bounds->x = getbegx(win);
+	bounds->y = getbegy(win);
+	bounds->w = getmaxx(win);
+	bounds->h = getmaxy(win);
 }
 
 void window_set_bounds(window_t* win, const rect_t* bounds) {
-	if (win->ref)
-		delwin(win->ref);
-
-	win->ref = newwin(bounds->h, bounds->w, bounds->y, bounds->x);
+	delwin(win);
+	window_t* tmp = newwin(bounds->h, bounds->w, bounds->y, bounds->x);
+	memcpy(win, tmp, sizeof(window_t));
 }
--- lib/window.h
+++ lib/window.h
@@ -1,24 +1,20 @@
 #ifndef OLEC_WINDOW_H
 #define OLEC_WINDOW_H
 
-#include "session.h"
 #include "rect.h"
+#include <ncurses.h>
 
-typedef struct _window {
-	struct _window* parent;
-	curswin* ref;
-} window_t;
+typedef WINDOW window_t;
 
 /**
  * Create a new window.
  */
-void window_create(window_t* win,
-                   size_t x, size_t y, size_t w, size_t h);
+window_t* window_new(size_t x, size_t y, size_t w, size_t h);
 
 /**
  * Delete a window.
  */
-void window_delete(const window_t* win);
+void window_free(window_t* win);
 
 /**
  * Extract the rectangle from a window.
