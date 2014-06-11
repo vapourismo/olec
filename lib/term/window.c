8ddb791ef2fe62bb1187b586a4435c9b8d155f6b
Ole <itstheimp@gmail.com>
2014-06-12 01:49:00 +0200

    window: revamped for a more manual way of handling windows

--- lib/term/window.c
+++ lib/term/window.c
@@ -1,28 +1,140 @@
 #include "window.h"
 #include "../aux.h"
+#include <string.h>
+#include <stdarg.h>
+#include <malloc.h>
+#include <ncurses.h>
+
+typedef WINDOW curswin_t;
 
 void window_create(window_t* win, size_t x, size_t y, size_t w, size_t h) {
-	*win = newwin(h, w, y, x);
+	win->x = x;
+	win->y = y;
+	win->w = w;
+	win->h = h;
 }
 
-void window_delete(window_t* win) {
-	if (*win) delwin(*win);
-	*win = NULL;
-}
+void window_fill(window_t* win, char c) {
+	for (size_t i = 0; i < win->h; i++) {
+		move(win->y + i, win->x);
 
-void window_get_bounds(const window_t* win, rect_t* bounds) {
-	window_t w = *win;
-	bounds->x = getbegx(w);
-	bounds->y = getbegy(w);
-	bounds->w = getmaxx(w);
-	bounds->h = getmaxy(w);
-}
-
-void window_set_bounds(window_t* win, const rect_t* bounds) {
-	if (*win) delwin(*win);
-	*win = newwin(bounds->h, bounds->w, bounds->y, bounds->x);
+		for (size_t j = 0; j < win->w; j++)
+			addch(c);
+	}
 }
 
 void window_clear(window_t* win) {
-	wclear(*win);
+	window_fill(win, ' ');
 }
+
+void window_move_cursor(window_t* win, size_t x, size_t y) {
+	if (x < win->w && y < win->y) {
+		move(win->y + y, win->x + x);
+	}
+}
+
+int window_encloses(window_t* win, size_t x, size_t y) {
+	return
+		x >= win->x &&
+		y >= win->y &&
+		x < win->x + win->w &&
+		y < win->y + win->h;
+}
+
+void window_draw_string(window_t* win, const char* message) {
+	/* retrieve the global cursor position */
+	size_t cur_x, cur_y;
+	getyx(stdscr, cur_y, cur_x);
+
+	/* if cursor is positioned within the window */
+	if (window_encloses(win, cur_x, cur_y)) {
+		size_t end_x = min(win->x + win->w,
+		                   cur_x + strlen(message));
+		addnstr(message, end_x - cur_x);
+	}
+}
+
+void window_draw_format(window_t* win, const char* format, ...) {
+	/* retrieve the global cursor position */
+	size_t cur_x, cur_y;
+	getyx(stdscr, cur_y, cur_x);
+
+	/* if the cursor is positioned within the window */
+	if (window_encloses(win, cur_x, cur_y)) {
+		size_t space = 1 + (win->x + win->w) - cur_x;
+
+		/* no need to write if there is no space */
+		if (space == 0) return;
+
+		char* buffer = (char*) malloc(space);
+
+		if (!buffer) return;
+
+		/* prepare the variadic argument list */
+		va_list vargs;
+		va_start(vargs, format);
+
+		/* format and write to screen if there is anything to be written */
+		ssize_t n = vsnprintf(buffer, space, format, vargs);
+		if (n > 0)
+			addnstr(buffer, n);
+
+		va_end(vargs);
+		free(buffer);
+	}
+}
+
+void window_render(const window_t* win) {
+	wredrawln(stdscr, win->y, win->h);
+}
+
+void window_vsplit_abs(const window_t* base,
+                       window_t* left, window_t* right,
+                       ssize_t sep) {
+	if (sep < 0)
+		sep = base->w - min(-sep, base->w);
+
+	/* place left */
+	left->x = base->x;
+	left->w = min(sep, base->w);
+
+	/* place right */
+	right->x = left->x + left->w;
+	right->w = base->w - left->w;
+
+	/* left and right have the same height and y-position */
+	left->y = right->y = base->y;
+	left->h = right->h = base->h;
+}
+
+void window_vsplit_rel(const window_t* base,
+                       window_t* left, window_t* right,
+                       double sep) {
+	window_vsplit_abs(base, left, right, base->w * sep);
+}
+
+void window_hsplit_abs(const window_t* base,
+                       window_t* top, window_t* bottom,
+                       ssize_t sep) {
+	if (sep < 0)
+		sep = base->h - min(-sep, base->h);
+
+	/* place top */
+	top->y = base->y;
+	top->h = min(sep, base->h);
+
+	/* place bottom */
+	bottom->y = top->y + top->h;
+	bottom->h = base->h - top->h;
+
+	/* top and bottom have the same width and x-position */
+	top->x = bottom->x = base->x;
+	top->w = bottom->w = base->w;
+}
+
+void window_hsplit_rel(const window_t* base,
+                       window_t* top, window_t* bottom,
+                       double sep) {
+	window_hsplit_abs(base, top, bottom, base->h * sep);
+}
+
--- lib/term/window.h
+++ lib/term/window.h
@@ -1,10 +1,12 @@
 #ifndef OLEC_WINDOW_H
 #define OLEC_WINDOW_H
 
-#include "rect.h"
-#include <ncurses.h>
+#include <stddef.h>
+#include <unistd.h>
 
-typedef WINDOW* window_t;
+typedef struct {
+	size_t x, y, w, h;
+} window_t;
 
 /**
  * Create a new window.
@@ -12,23 +14,66 @@ typedef WINDOW* window_t;
 void window_create(window_t* win, size_t x, size_t y, size_t w, size_t h);
 
 /**
- * Delete a window.
+ * Fill a window with the given character.
  */
-void window_delete(window_t* win);
+void window_fill(window_t* win, char c);
 
 /**
- * Extract the rectangle from a window.
- */
-void window_get_bounds(const window_t* win, rect_t* bounds);
-
-/**
- * Set the bounds.
- */
-void window_set_bounds(window_t* win, const rect_t* bounds);
-
-/**
- * Clear the window contents
+ * Clear the window contents.
  */
 void window_clear(window_t* win);
+
+/**
+ * Position the cursor within the window.
+ */
+void window_move_cursor(window_t* win, size_t x, size_t y);
+
+/**
+ * Draw a string.
+ */
+void window_draw_string(window_t* win, const char* message);
+
+/**
+ * Draw a string using a format.
+ */
+void window_draw_format(window_t* win, const char* format, ...);
+
+/**
+ * Checks if the given coordinates are within 'win'.
+ */
+int window_encloses(window_t* win, size_t x, size_t y);
+
+/**
+ * Flush the changes in win to the screen.
+ */
+void window_render(const window_t* win);
+
+/**
+ * Split a window vertically (absolute)
+ */
+void window_vsplit_abs(const window_t* base,
+                       window_t* left, window_t* right,
+                       ssize_t sep);
+
+/**
+ * Split a window vertically (relative)
+ */
+void window_vsplit_rel(const window_t* base,
+                       window_t* left, window_t* right,
+                       double sep);
+
+/**
+ * Split a window horizontally (absolute)
+ */
+void window_hsplit_abs(const window_t* base,
+                       window_t* top, window_t* bottom,
+                       ssize_t sep);
+
+/**
+ * Split a window vertically (relative)
+ */
+void window_hsplit_rel(const window_t* base,
+                       window_t* top, window_t* bottom,
+                       double sep);
 
 #endif
