f355c3123c2c73c9524dcba13e6ce395d5b9f952
Ole <itstheimp@gmail.com>
2014-06-12 02:06:39 +0200

    layout: changes based refactoring window

--- lib/term/layout.c
+++ lib/term/layout.c
@@ -2,10 +2,12 @@
 #include "../aux.h"
 #include <stdarg.h>
 
-void layout_new_vargs(layout_t* lay, const rect_t* bounds, split_mode_t mode, va_list vargs) {
-	if (bounds) lay->bounds = *bounds;
+void layout_create(layout_t* lay, const window_t* win, split_mode_t mode, ...) {
 	lay->mode = mode;
-	lay->a = lay->b = NULL;
+	lay->parent = win;
+
+	va_list vargs;
+	va_start(vargs, mode);
 
 	/* determine which kind of argument needs to be extracted from
 	   the variable arguments.
@@ -15,78 +17,29 @@ void layout_new_vargs(layout_t* lay, const rect_t* bounds, split_mode_t mode, va
 	else
 		lay->seperator.relative = va_arg(vargs, double);
 
+	va_end(vargs);
+
 	/* update/create the underlying windows */
 	layout_update(lay);
 }
 
-void layout_new(layout_t* lay, const rect_t* bounds, split_mode_t mode, ...) {
-	va_list vargs;
-	va_start(vargs, mode);
-
-	/* there is no parent container */
-	lay->parent = NULL;
-
-	/* let 'layout_new_vargs' initialize the structure */
-	layout_new_vargs(lay, bounds, mode, vargs);
-
-	va_end(vargs);
-}
-
-void layout_sub_win(layout_t* lay, const window_t* target, split_mode_t mode, ...) {
-	va_list vargs;
-	va_start(vargs, mode);
-
-	/* reference the parent container */
-	lay->parent = target;
-
-	/* let 'layout_new_vargs' initialize the structure */
-	layout_new_vargs(lay, NULL, mode, vargs);
-
-	va_end(vargs);
-}
-
-void layout_free(layout_t* lay) {
-	if (lay) {
-		window_delete(&lay->a);
-		window_delete(&lay->b);
-	}
-}
-
 void layout_update(layout_t* lay) {
-	if (lay->parent) {
-		/* update the window bounds if this layout is part of another window */
-		window_get_bounds(lay->parent, &lay->bounds);
-	}
-
-	rect_t ra, rb;
-
 	/* calculate both window bounds based on the splitting mode */
 	switch (lay->mode) {
 		case VSPLIT_ABS:
-			rect_vsplit_abs(&lay->bounds, &ra, &rb, lay->seperator.absolute);
+			window_vsplit_abs(lay->parent, &lay->a, &lay->b, lay->seperator.absolute);
 			break;
 
 		case HSPLIT_ABS:
-			rect_hsplit_abs(&lay->bounds, &ra, &rb, lay->seperator.absolute);
+			window_hsplit_abs(lay->parent, &lay->a, &lay->b, lay->seperator.absolute);
 			break;
 
 		case VSPLIT_REL:
-			rect_vsplit_rel(&lay->bounds, &ra, &rb, lay->seperator.relative);
+			window_vsplit_rel(lay->parent, &lay->a, &lay->b, lay->seperator.relative);
 			break;
 
 		case HSPLIT_REL:
-			rect_hsplit_rel(&lay->bounds, &ra, &rb, lay->seperator.relative);
+			window_hsplit_rel(lay->parent, &lay->a, &lay->b, lay->seperator.relative);
 			break;
 	}
-
-	/* update both window bounds */
-	window_set_bounds(&lay->a, &ra);
-	window_set_bounds(&lay->b, &rb);
-}
-
-void layout_set_bounds(layout_t* lay, size_t x, size_t y, size_t w, size_t h) {
-	lay->bounds.x = x;
-	lay->bounds.y = y;
-	lay->bounds.w = w;
-	lay->bounds.h = h;
 }
--- lib/term/layout.h
+++ lib/term/layout.h
@@ -3,7 +3,6 @@
 
 #include "session.h"
 #include "window.h"
-#include "rect.h"
 
 typedef enum {
 	VSPLIT_REL,
@@ -14,40 +13,23 @@ typedef enum {
 
 typedef struct {
 	split_mode_t mode;
-	rect_t bounds;
 	const window_t* parent;
+	window_t a, b;
 
 	union {
 		ssize_t absolute;
 		double relative;
 	} seperator;
-
-	window_t a, b;
 } layout_t;
-
-/**
- * Create a new layout container.
- */
-void layout_new(layout_t* lay, const rect_t* bounds, split_mode_t mode, ...);
 
 /**
  * Create a new layout container within an existing window.
  */
-void layout_sub_win(layout_t* lay, const window_t* source, split_mode_t mode, ...);
-
-/**
- * Free an existing layout container.
- */
-void layout_free(layout_t* lay);
+void layout_create(layout_t* lay, const window_t* win, split_mode_t mode, ...);
 
 /**
  * Resize the underlying windows to match the layout bounds.
  */
 void layout_update(layout_t* lay);
-
-/**
- * Update the layout bounds manually.
- */
-void layout_set_bounds(layout_t* lay, size_t x, size_t y, size_t w, size_t h);
 
 #endif
