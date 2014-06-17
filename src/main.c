f18ba7727606d28a09ddd1081abbb6d947651994
Ole <itstheimp@gmail.com>
2014-06-17 20:49:32 +0200

    main: editor usage

--- src/main.c
+++ src/main.c
@@ -1,24 +1,39 @@
 #include "../lib/log.h"
 #include "../lib/term/session.h"
-#include "../lib/term/window.h"
-#include "../lib/term/layout.h"
 #include "../lib/term/views/mainview.h"
+#include "../lib/term/views/editor.h"
 #include <ncurses.h>
 
-void render_viewport(window_t* target) {
-	window_fill(target, '#');
-}
+#define COLFAC (100 / 255)
 
 int main(int argc, char** argv) {
 	/* init app */
 	log_open("application.log");
 	session_start();
 
+	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
+	init_pair(3, COLOR_BLUE, COLOR_WHITE);
+
 	mainview_t mview;
 	mainview_create(&mview);
 
-	render_viewport(mview.viewport);
+	editor_t ed;
+	ed.linenum_padding = 1;
+	ed.linenum_min_width = 2;
+	ed.linenum_color_normal = 2;
+	ed.linenum_color_selected = 3;
+	ed.text_margin = 1;
+
+	editor_create(&ed, mview.viewport);
+
+	ed.render_from = 1;
+	ed.render_to = ed.lay_split.a.h;
+	ed.lines = ed.lay_split.a.h;
+	ed.current_line = 3;
+	editor_update(&ed);
+
 	window_fill(mview.statusbar, '2');
+	editor_render(&ed);
 
 	session_render();
 	fgetc(stdin);
