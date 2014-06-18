7431e5f9954948f351b15a00a074640924cd22a5
Ole <itstheimp@gmail.com>
2014-06-19 00:14:01 +0200

    main: new line splitting example

--- src/main.c
+++ src/main.c
@@ -6,16 +6,21 @@ int main(void) {
 	/* init app */
 	log_open("application.log");
 
-	line_t line;
-	line_create(&line);
+	line_t line1, line2;
+	line_create(&line1);
 
-	line_append_many(&line, "Heo", 3);
-	line_insert_many(&line, 2, "ll", 2);
+	line_append_many(&line1, "Heo", 3);
+	line_insert_many(&line1, 2, "ll", 2);
 
-	line_append(&line, 0);
-	puts(line.buffer);
+	line_split(&line1, 2, &line2);
 
-	line_destroy(&line);
+	line_append(&line1, 0);
+	line_append(&line2, 0);
+
+	puts(line1.buffer);
+	puts(line2.buffer);
+
+	line_destroy(&line1);
 
 	/* finalize */
 	log_close();
