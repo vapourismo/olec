48afa3d45c884065a6b7eb6515cf3f91be0c8d57
Ole <itstheimp@gmail.com>
2014-06-18 23:52:09 +0200

    main: line buffer/editor example

--- src/main.c
+++ src/main.c
@@ -1,39 +1,23 @@
 #include "../lib/log.h"
-#include "../lib/term/session.h"
-#include "../lib/term/views/mainview.h"
-#include "../lib/term/views/editor.h"
-#include <ncurses.h>
+#include "../lib/aux.h"
+#include "../lib/text/line.h"
 
-#define COLFAC (100 / 255)
-
-int main(int argc, char** argv) {
+int main(void) {
 	/* init app */
 	log_open("application.log");
-	session_start();
 
-	init_pair(1, COLOR_BLACK, COLOR_WHITE);
-	init_pair(2, COLOR_YELLOW, COLOR_BLACK);
-	init_pair(3, COLOR_BLUE, COLOR_WHITE);
+	line_t line;
+	line_create(&line);
 
-	mainview_t mview;
-	mainview_create(&mview);
+	line_append_many(&line, "Heo", 3);
+	line_insert_many(&line, 2, "ll", 2);
 
-	editor_t ed;
-	editor_create(&ed, mview.viewport);
+	line_append(&line, 0);
+	puts(line.buffer);
 
-	ed.num_lines = 40;
-
-	attrset(COLOR_PAIR(1));
-	window_fill(mview.statusbar, ' ');
-	attrset(0);
-
-	editor_render(&ed);
-
-	session_render();
-	fgetc(stdin);
+	line_destroy(&line);
 
 	/* finalize */
-	session_stop();
 	log_close();
 
 	return 0;
