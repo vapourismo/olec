3ee9a0621b8cb439f6ce9f51eefc11abfe7fb7bd
Ole <itstheimp@gmail.com>
2014-06-22 23:43:13 +0200

    window: added functions to draw within a window

--- src/Olec/Terminal/Window.hs
+++ src/Olec/Terminal/Window.hs
@@ -1,11 +1,15 @@
 module Olec.Terminal.Window (Window, wMoveCursor,
                              wDimension, wOrigin,
+                             wDrawString, wDrawByteString, wDrawByteString8,
                              defaultWindow, nullWindow,
                              newWindow, subWindow,
                              fillWindow) where
 
 import Olec.Terminal
 import Control.Monad
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
 
 -- Data types
 type Window = (Int, Int, Int, Int)
@@ -21,6 +25,50 @@ wMoveCursor :: Window -> Position -> IO ()
 wMoveCursor (wx, wy, ww, wh) (x, y)
 	| x < ww && y < wh = moveCursor (wx + x) (wy + y)
 wMoveCursor _ _ = return ()
+
+-- | Draw a String within a Window.
+wDrawString :: Window -> String -> IO ()
+wDrawString (winX, winY, winW, winH) str = do
+	(x, y) <- cursor
+	when (y >= winY && y < winY + winH && x < winX + winW && x + length str >= winX) $ do
+		let (cutFront, cutBack, curX) = wFitEntity x winX winW (length str)
+		let result = drop cutFront (take (length str - cutBack) str)
+		when (length result > 0) $ do
+			moveCursor curX y
+			drawString result
+
+-- | Draw a ByteString within a Window.
+wDrawByteString :: Window -> B.ByteString -> IO ()
+wDrawByteString (winX, winY, winW, winH) str = do
+	(x, y) <- cursor
+	when (y >= winY && y < winY + winH && x < winX + winW && x + B.length str >= winX) $ do
+		let (cutFront, cutBack, curX) = wFitEntity x winX winW (B.length str)
+		let result = B.drop cutFront (B.take (B.length str - cutBack) str)
+		when (B.length result > 0) $ do
+			moveCursor curX y
+			drawByteString result
+
+-- | Draw a ByteString.Char8 within a Window.
+wDrawByteString8 :: Window -> C.ByteString -> IO ()
+wDrawByteString8 (winX, winY, winW, winH) str = do
+	(x, y) <- cursor
+	when (y >= winY && y < winY + winH && x < winX + winW && x + C.length str >= winX) $ do
+		let (cutFront, cutBack, curX) = wFitEntity x winX winW (C.length str)
+		let result = C.drop cutFront (C.take (C.length str - cutBack) str)
+		when (C.length result > 0) $ do
+			moveCursor curX y
+			drawByteString8 result
+
+-- | Fit an entity inside a Window.
+wFitEntity :: Int -> Int -> Int -> Int -> (Int, Int, Int)
+wFitEntity x winX winW len =
+	(cutFront, cutBack, max winX (min (winX + winW) x)) where
+		cutFront = if x < winX
+			then winX - x
+			else 0
+		cutBack = if x + len > winX + winW
+			then (x + len) - (winX + winW)
+			else 0
 
 -- | Retrieve the window's origin.
 wOrigin :: Window -> Position
@@ -56,4 +104,3 @@ fillWindow :: Window -> Char -> IO ()
 fillWindow win c = forM_ [0 .. (h - 1)] (renderLine $ replicate w c) where
 	(w, h) = wDimension win
 	renderLine line y = wMoveCursor win (0, y) >> drawString line
-
