e43e57a2d7bcd7d5151b781dd0915d8472dbdaa2
Ole <itstheimp@gmail.com>
2014-06-22 21:46:41 +0200

    main: example

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,13 @@
 import Olec.Terminal
 import Olec.Terminal.Window
+import Olec.Terminal.Layout
 
 main = withTerminal $ do
 	scr <- defaultWindow
-	let win = subWindow scr 0 0 10 10
-	fillWindow win '#'
+	let (win1, win2) = split (RelVSplit (-0.25)) scr
+
+	fillWindow win1 '1'
+	fillWindow win2 '2'
+
 	render
 	getChar
