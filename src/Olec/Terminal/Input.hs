57e44b73fac1186b6ae7fa9ae8f56e53d2d911e3
Ole <itstheimp@gmail.com>
2014-06-23 21:16:15 +0200

    input: first draft of the input managing functions

---
+++ src/Olec/Terminal/Input.hs
@@ -0,0 +1,56 @@
+module Olec.Terminal.Input (KeyStroke (..), readKey) where
+
+import Data.Char
+import System.IO (stdin)
+import qualified Data.ByteString.Char8 as B
+
+-- | Key Modifer
+data KeyModifier = KeyCtrl | KeyCtrlAlt | KeyAlt | KeyNoMod
+	deriving (Show, Eq)
+
+-- | Special Keys
+data SpecialKey = KeyReturn
+                | KeyEscape
+	deriving (Show, Eq)
+
+-- | Key Stroke
+data KeyStroke = KeyPrint     KeyModifier Char
+               | KeySpecial   KeyModifier SpecialKey
+               | KeyUndefined [Char]
+	deriving (Show, Eq)
+
+-- Is c control modified?
+isCtrlMod c = 1 <= ord c && ord c <= 26
+
+-- Fix the control modifier offset
+fixCtrlOffset c = chr (ord c + 96)
+
+-- Special sequences
+parseInput "\r"       = KeySpecial KeyNoMod KeyReturn
+parseInput "\n"       = KeySpecial KeyCtrl  KeyReturn
+parseInput "\ESC\n"   = KeySpecial KeyAlt   KeyReturn
+parseInput "\ESC\r"   = KeySpecial KeyAlt   KeyReturn
+parseInput "\ESC"     = KeySpecial KeyNoMod KeyEscape
+parseInput "\ESC\ESC" = KeySpecial KeyAlt   KeyEscape
+
+-- Ordinary characters
+parseInput (x : [])
+	| isPrint x       = KeyPrint KeyNoMod x
+	| isCtrlMod x     = KeyPrint KeyCtrl  (fixCtrlOffset x)
+
+-- Alt + Ordinary character
+parseInput ('\ESC' : x : [])
+	| isPrint x       = KeyPrint KeyAlt     x
+	| isCtrlMod x     = KeyPrint KeyCtrlAlt (fixCtrlOffset x)
+
+-- Anything else
+parseInput xs         = KeyUndefined xs
+
+-- | Read a KeyStroke.
+readKey :: IO KeyStroke
+readKey = do
+	input <- B.hGetSome stdin 10
+	return $ if B.length input > 0
+		then parseInput (B.unpack input)
+		else KeyUndefined []
+
