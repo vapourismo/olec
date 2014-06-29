962bc6f21dc2a0cd4f0c40decb8dc276cfb5fb31
Ole <itstheimp@gmail.com>
2014-06-30 01:02:36 +0200

    main: appstate example

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,34 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 import Olec.Terminal
+import Olec.Terminal.Window
+import Olec.Terminal.Layout
 import Olec.Terminal.Input
+
+import Control.Monad.State
+
+data AppState = forall a. AppState { statusbar :: Window
+                                   , viewport  :: Window
+                                   , content   :: Window -> IO a }
+
+type AppStateT = StateT AppState IO
+
+updateLayout :: AppStateT ()
+updateLayout = do
+	(wViewport, wStatusbar) <- liftIO (fmap (split $ AbsHSplit (-1)) defaultWindow)
+	modify (\app -> app {statusbar = wStatusbar, viewport = wViewport})
+
+renderViewport :: AppStateT ()
+renderViewport = do
+	AppState _ vp cnt <- get
+	liftIO (cnt vp >> render)
+
+initAppState = AppState nullWindow nullWindow (\w -> wFill w '#')
 
 main = withTerm $ do
 	src <- processInput
-	render
+	flip execStateT initAppState $ do
+		updateLayout
+		renderViewport
 
-	let loop = do
-		ev <- readInputEvent src
-		case ev of
-			KeyPress ModControl (KeyChar 'q') -> return ()
-			event -> do
-				drawString (show event)
-				drawString "; "
-				render
-				loop
-
-	loop
+	readInputEvent src
