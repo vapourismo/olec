8cea2d22079ef7805f1394ce8a55d43e8f340dc8
Ole <itstheimp@gmail.com>
2014-09-03 17:24:07 +0200

    Updated for pushing

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,6 @@ import Control.Monad
 import Olec.Terminal.Bindings
 import Olec.Terminal.Input
 import Olec.Visual
-import Olec.BoxManager
 
 
 fill :: Canvas a => a -> Char -> IO ()
@@ -20,65 +19,5 @@ fill c chr = do
 main :: IO ()
 main = withTerm $ do
 	input <- processInput
-	(w, h) <- termSize
-
-	win1 <- newWindow (0, 0) (5, 5)
-	win2 <- newWindow (5, 5) (5, 5)
-	win3 <- newWindow (10, 10) (5, 5)
-
-	fill win1 '1'
-	fill win2 '2'
-	fill win3 '3'
-
-	render win1
-	render win2
-	render win3
-
-	void (readInputEvent input)
-
-	clear win1
-	clear win2
-	clear win3
-
-	render win1
-	render win2
-	render win3
-
-	let wm =
-		SplitVP 0.5
-		        (Single win1)
-		        (SplitHP 0.5
-		                 (Single win2)
-		                 (Single win3))
-
-	setBounds wm (0, 0, w, h)
-
-	fill win1 '1'
-	fill win2 '2'
-	fill win3 '3'
-
-	render win1
-	render win2
-	render win3
-
-	void (readInputEvent input)
-
-	clear win1
-	clear win2
-	clear win3
-
-	render win1
-	render win2
-	render win3
-
-	setBounds wm (0, 0, div w 2, div h 2)
-
-	fill win1 '1'
-	fill win2 '2'
-	fill win3 '3'
-
-	render win1
-	render win2
-	render win3
 
 	void (readInputEvent input)
