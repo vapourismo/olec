8f454a01976e2e764434d9039499818e146514ff
Ole Krüger <ole@vprsm.de>
2015-03-12 02:26:58 +0100

    Allow reload by signal

--- src/child.c
+++ src/child.c
@@ -2,6 +2,11 @@
 #include "olec.h"
 
 #include <stdlib.h>
+#include <signal.h>
+
+static void handle_signal(int sig) {
+	exit(OLEC_CHILD_EXIT_RELOAD);
+}
 
 int olec_child_launch(int argc, char** argv) {
 	// Setup environment
@@ -10,6 +15,8 @@ int olec_child_launch(int argc, char** argv) {
 
 	if (!event_source || !olec_init(&olec, event_source))
 		return OLEC_CHILD_EXIT_ERROR;
+
+	signal(SIGUSR1, handle_signal);
 
 	return olec_main(&olec);
 }
