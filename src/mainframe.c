4274066e9f9af0c8006b720fec81babec2cd2a61
Ole Krüger <ole@vprsm.de>
2015-03-14 00:38:06 +0100

    Status bar

--- src/mainframe.c
+++ src/mainframe.c
@@ -16,8 +16,13 @@ void olec_main_frame_update(OlecMainFrame* frame) {
 		delwin(frame->status);
 
 	olec_main_frame_init(frame);
+	clear();
 }
 
 void olec_main_frame_render(const OlecMainFrame* frame) {
+	mvwprintw(frame->status, 0, 1, "[%ix%i] Put status bar contents here",
+	          getmaxx(frame->status), getmaxy(frame->status));
+	mvwchgat(frame->status, 0, 0, getmaxx(frame->status), 0, 1, NULL);
+
 	refresh();
 }
