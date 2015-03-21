d3776538badbdd0c2d6754519446eb954ed3d30c
Ole Krüger <ole@vprsm.de>
2015-03-21 18:30:34 +0100

    This should have been committed earlier

--- src/js/vm.cc
+++ src/js/vm.cc
@@ -1,6 +1,8 @@
 #include "vm.h"
 
 #include <iostream>
+#include <map>
+#include <string>
 #include <v8.h>
 
 using namespace std;
@@ -19,13 +21,13 @@ struct V8Initializer {
 	}
 } v8init;
 
-static
-Value js_require(String a) {
-	ScriptFile script(a.c_str());
-	script.run();
+// static
+// Value js_require(String a) {
+// 	ScriptFile script(a.c_str());
+// 	script.run();
 
-	return script.exports();
-}
+// 	return script.exports();
+// }
 
 static
 void js_debug_log(const v8::FunctionCallbackInfo<v8::Value>& args) {
@@ -40,10 +42,38 @@ void js_debug_log(const v8::FunctionCallbackInfo<v8::Value>& args) {
 	cout << endl;
 }
 
+template <typename T, typename... A> static
+v8::Local<v8::Object> new_object(ObjectTemplate& object_tpl, A... args) {
+	object_tpl->SetInternalFieldCount(1);
+
+	// Construct C++ type
+	T* instance = new T(args...);
+	v8::Local<v8::External> self = v8::External::New(object_tpl.isolate, instance);
+
+	// Instantiate object
+	v8::Local<v8::Object> obj = object_tpl->NewInstance();
+	obj->SetInternalField(0, self);
+
+	return obj;
+}
+
+struct ModuleSystem {
+	map<string, v8::Local<v8::Value>> modules;
+
+	ModuleSystem() {
+
+	}
+
+	Value require(String file) {
+		return Foreign<String>::generate(EngineInstance::current(), file);
+	}
+};
+
 EngineInstance::EngineInstance():
 	isolate(v8::Isolate::New()),
 	isolate_scope(isolate.get()),
 	handle_scope(isolate.get()),
+	global_context(isolate.get()),
 	global_template(isolate.get())
 {
 	isolate->SetData(0, this);
@@ -52,9 +82,20 @@ EngineInstance::EngineInstance():
 	ObjectTemplate debug_tpl(isolate.get());
 	debug_tpl.set("log", v8::FunctionTemplate::New(isolate.get(), js_debug_log));
 
+	// Module Class
+	// ObjectTemplate module_tpl(isolate.get());
+
+	// MethodTemplate<ModuleSystem, Value, String> module_require_tpl(isolate.get(), &ModuleSystem::require);
+	// module_tpl.set("require", module_require_tpl);
+
+	ClassTemplate<ModuleSystem> module_tpl(isolate.get());
+	module_tpl.method("require", &ModuleSystem::require);
+
 	// Register fields
 	global_template.set("debug", debug_tpl);
-	global_template.set("require", js_require);
+	global_template.set("module", module_tpl.instantiate());
+	// global_template.set("module", new_object<ModuleSystem>(module_tpl));
+	// global_template.set("require", js_require);
 }
 
 }
