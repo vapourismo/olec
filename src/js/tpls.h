f2f23e47be26dfd024ebe8cf62bcaeeb0795d20c
Ole Krüger <ole@vprsm.de>
2015-03-21 18:31:19 +0100

    Allow ClassTemplates to instantiate the underlying class

--- src/js/tpls.h
+++ src/js/tpls.h
@@ -333,6 +333,21 @@ struct ClassTemplate: v8::Local<v8::FunctionTemplate> {
 		                      _getter<AR>, _setter<AR>,
 		                      v8::External::New(isolate, new _property_wrapper<AR> {property}));
 	}
+
+	/**
+	 * Instantiate this class
+	 */
+	v8::Local<v8::Object> instantiate(A... args) {
+		// Construct C++ type
+		v8::Local<v8::External> self = v8::External::New(isolate, new T(args...));
+
+		// Instantiate object
+		v8::Local<v8::Object> obj = instance->NewInstance();
+		obj->SetPrototype(prototype->NewInstance());
+		obj->SetInternalField(0, self);
+
+		return obj;
+	}
 };
 
 }
