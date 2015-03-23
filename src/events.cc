aa9b9c63366a4d3b376a3014ca05665e1d2bea53
Ole Krüger <ole@vprsm.de>
2015-03-23 01:05:31 +0100

    Fix several event bugs

--- src/events.cc
+++ src/events.cc
@@ -19,21 +19,8 @@ bool send_entirely(int fd, const void* data, size_t rem) {
 	return true;
 }
 
-bool receive_entirely(int fd, void* data, size_t rem) {
-	uint8_t* pos = (uint8_t*) data;
-	uint8_t* end = pos + rem;
-
-	while (pos != end) {
-		ssize_t r = ::recv(fd, pos, rem, 0);
-
-		if (r < 0)
-			return false;
-
-		pos += r;
-		rem -= r;
-	}
-
-	return true;
+bool receive_entirely(int fd, void* data, size_t len) {
+	return ::recv(fd, data, len, MSG_WAITALL) == signed(len);
 }
 
 }
--- src/events.h
+++ src/events.h
@@ -3,7 +3,6 @@
 
 #include <string>
 #include <list>
-#include <iostream>
 #include <thread>
 #include <cstdint>
 #include <cassert>
@@ -76,62 +75,30 @@ bool receive_entirely(int fd, void* data, size_t rem);
  */
 template <typename T>
 struct EventHub {
-	struct Listener {
-		int fd;
-		event* ev;
-
-		Listener(int fd, EventHub* parent):
-			fd(fd),
-			ev(event_new(parent->ev_base, fd, EV_PERSIST | EV_READ,
-			             (event_callback_fn) EventHub::dispatch_read, parent))
-		{
-			assert(ev != nullptr);
-			event_add(ev, nullptr);
-		}
-
-		~Listener() {
-			event_del(ev);
-			event_free(ev);
-
-			if (fd >= 0) {
-				close(fd);
-			}
-		}
-	};
-
 	static
-	void dispatch_read(int fd, short, EventHub* self) {
+	void dispatch_read(int fd, short, EventHub<T>* self) {
 		T data;
 		if (receive_entirely(fd, &data, sizeof(T))) {
 			self->handle(data);
-
-			// Notify all other nodes
-			for (auto it = self->listeners.begin(); it != self->listeners.end(); it++) {
-				if (it->fd != fd && !send_entirely(it->fd, &data, sizeof(T))) {
-					// If transmission failed, remove listener
-					self->listeners.erase(it);
-				}
-			}
-		} else {
-			// Remove listener
-			for (auto it = self->listeners.begin(); it != self->listeners.end(); it++) {
-				if (it->fd == fd) {
-					self->listeners.erase(it);
-				}
-			}
 		}
 	}
 
 	static
-	void dispatch_accept(int fd, short, EventHub* self) {
+	void dispatch_accept(int fd, short, EventHub<T>* self) {
 		int cl = accept(fd, nullptr, nullptr);
 		evutil_make_socket_nonblocking(cl);
-		self->listeners.emplace_back(cl, self);
+
+		event* ev_read =
+			event_new(self->ev_base, cl, EV_PERSIST | EV_READ,
+			          (event_callback_fn) dispatch_read, self);
+		event_add(ev_read, NULL);
+
+		self->listeners.push_back(ev_read);
 	}
 
 	int fd;
 	std::string path;
-	std::list<Listener> listeners;
+	std::list<event*> listeners;
 
 	event_base* ev_base;
 	event* ev_accept;
@@ -162,21 +129,16 @@ struct EventHub {
 		ev_base = event_base_new();
 		assert(ev_base != nullptr);
 
-		ev_accept = event_new(ev_base, fd, EV_PERSIST | EV_READ,
-		                      (event_callback_fn) dispatch_accept, this);
+		ev_accept = event_new(ev_base, fd, EV_PERSIST | EV_READ, (event_callback_fn) dispatch_accept, this);
 		assert(ev_accept != nullptr);
-
-		event_add(ev_accept, nullptr);
 	}
 
 	~EventHub() {
-		// Remove all listeners
-		listeners.clear();
+		// Free all listeners
+		for_each(listeners.begin(), listeners.end(), event_free);
 
-		// Remove and free events
-		event_del(ev_accept);
+		// Free events
 		event_free(ev_accept);
-
 		event_base_free(ev_base);
 
 		// Close socket and remove occupied unix socket
@@ -185,21 +147,22 @@ struct EventHub {
 	}
 
 	void dispatch() {
+		event_add(ev_accept, nullptr);
 		event_base_dispatch(ev_base);
 	}
 
 	void notify(const T& data) {
 		// Send message to every node
-		for (auto it = listeners.begin(); it != listeners.end(); it++) {
-			if (!send_entirely(it->fd, &data, sizeof(T))) {
-				// If transmission failed, remove listener
-				listeners.erase(it);
-			}
+		for (auto it: listeners) {
+			send_entirely(event_get_fd(it), &data, sizeof(T));
 		}
 	}
 
 	void exit() {
-		event_base_loopexit(ev_base, NULL);
+		event_del(ev_accept);
+
+		// Remove all listeners
+		for_each(listeners.begin(), listeners.end(), event_del);
 	}
 
 	virtual
@@ -247,13 +210,10 @@ struct EventNode {
 		ev_read = event_new(ev_base, fd, EV_PERSIST | EV_READ,
 		                    (event_callback_fn) dispatch_read, this);
 		assert(ev_read != nullptr);
-
-		event_add(ev_read, NULL);
 	}
 
 	~EventNode() {
 		// Remove events
-		event_del(ev_read);
 		event_free(ev_read);
 		event_base_free(ev_base);
 
@@ -263,6 +223,7 @@ struct EventNode {
 	}
 
 	void dispatch() {
+		event_add(ev_read, NULL);
 		event_base_dispatch(ev_base);
 	}
 
@@ -271,7 +232,7 @@ struct EventNode {
 	}
 
 	void exit() {
-		event_base_loopexit(ev_base, NULL);
+		event_del(ev_read);
 	}
 
 	virtual
@@ -305,6 +266,8 @@ struct ThreadedEventHub: _EvThreadSetup, EventHub<T> {
 		EventHub<T>::exit();
 		worker.join();
 	}
+
+	void exit() = delete;
 };
 
 }
