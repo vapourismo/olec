bd7d0bcb980f76837afcf1423e3f9db521b6cb6f
Ole Krüger <ole@vprsm.de>
2015-03-23 01:08:11 +0100

    Use new event system

--- src/app.cc
+++ src/app.cc
@@ -6,83 +6,56 @@
 
 namespace olec {
 
-Application::Application(const std::string& ipc_path) throw (Application::Error):
-	ev_base(event_base_new()),
-	ipc_fd(open(ipc_path.c_str(), O_RDONLY | O_NONBLOCK))
-{
-	if (!ev_base)
-		throw EventBaseAllocFailed;
-
-	if (ipc_fd < 0)
-		throw CommOpenFailed;
-}
-
-Application::~Application() {
-	if (ev_base)
-		event_base_free(ev_base);
-
-	if (ipc_fd >= 0)
-		close(ipc_fd);
-}
-
 static
-void cb_event_dispatch(int data, short what, Application* app) throw (Application::Error) {
+void cb_event_dispatch(int data, short what, Application* app) {
 	if (what == EV_SIGNAL && data == SIGWINCH) {
 		// Handle window resize
 		winsize ws;
 		if (ioctl(STDIN_FILENO, TIOCGWINSZ, &ws) != 0)
 			return;
 
-		app->handle_resize(ws);
+		app->resize(ws);
 	} else if (what == EV_SIGNAL && data == SIGUSR1) {
 		// Reload application
 		app->exit(1);
-	} else if (what == EV_READ && data == app->ipc_fd) {
-		// Fetch input event
-		Event ev;
-
-		if (!ipc_receive(app->ipc_fd, ev)) {
-			app->exit(2);
-		} else {
-			app->handle_event(ev);
-		}
 	}
 }
 
-int Application::main() throw (Application::Error) {
+Application::Application(const std::string& ipc_path):
+	EventNode<Event>(ipc_path)
+{
 	// Allocate events
-	event* ev_input =
-		event_new(ev_base, ipc_fd, EV_PERSIST | EV_READ,
-		          (event_callback_fn) cb_event_dispatch, this);
-	event* ev_resize =
+	ev_resize =
 		event_new(ev_base, SIGWINCH, EV_PERSIST | EV_SIGNAL,
 		          (event_callback_fn) cb_event_dispatch, this);
-	event* ev_reload =
+	ev_reload =
 		event_new(ev_base, SIGUSR1, EV_PERSIST | EV_SIGNAL,
 		          (event_callback_fn) cb_event_dispatch, this);
+}
 
-	if (!ev_input || !ev_resize || !ev_reload)
-		throw EventAllocFailed;
+Application::~Application() {
+	// Free events
+	event_free(ev_resize);
+	event_free(ev_reload);
+}
 
+void Application::exit(int status) {
+	exit_status = status;
+
+	// Remove events
+	event_del(ev_resize);
+	event_del(ev_reload);
+
+	EventNode<Event>::exit();
+}
+
+void Application::main() {
 	// Add events
-	event_add(ev_input, nullptr);
 	event_add(ev_resize, nullptr);
 	event_add(ev_reload, nullptr);
 
 	// Dispatch events
-	event_base_dispatch(ev_base);
-
-	// Remove events
-	event_del(ev_input);
-	event_del(ev_resize);
-	event_del(ev_reload);
-
-	// Free events
-	event_free(ev_input);
-	event_free(ev_resize);
-	event_free(ev_reload);
-
-	return exit_status;
+	dispatch();
 }
 
 }
--- src/app.h
+++ src/app.h
@@ -1,7 +1,7 @@
 #ifndef OLEC_APP_H_
 #define OLEC_APP_H_
 
-#include "ipc.h"
+#include "events.h"
 #include "keymap.h"
 
 #include <string>
@@ -11,36 +11,23 @@
 
 namespace olec {
 
-struct Application {
-	enum Error {
-		EventBaseAllocFailed,
-		EventAllocFailed,
-		EventDispatchFailed,
-		CommOpenFailed
-	};
-
-	event_base* ev_base;
-	int ipc_fd;
+struct Application: EventNode<Event> {
 	int exit_status = 0;
 
-	Application(const std::string& ipc_path) throw (Error);
+	event* ev_resize;
+	event* ev_reload;
+
+	Application(const std::string& ipc_path);
 
 	virtual
 	~Application();
 
-	int main() throw (Error);
+	void main();
 
-	inline
-	void exit(int status = 0) {
-		exit_status = status;
-		event_base_loopbreak(ev_base);
-	}
+	void exit(int status = 0);
 
 	virtual
-	void handle_event(const Event& ev) = 0;
-
-	virtual
-	void handle_resize(const winsize& ws) = 0;
+	void resize(const winsize& ws) = 0;
 };
 
 }
