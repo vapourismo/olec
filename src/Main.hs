20715f7212864f764e72b2061f9c3572cfd7b66a
Ole Krüger <ole@vprsm.de>
2015-04-15 01:54:46 +0200

    Haskell only version

--- src/Main.hs
+++ src/Main.hs
@@ -2,89 +2,34 @@
 
 module Main (main) where
 
---import Control.Monad
---import Control.Concurrent
-
---import qualified Data.ByteString as B
-
---import Foreign.C
---import Foreign.Marshal
---import Foreign.Ptr
-
---import System.Posix.IO
---import System.Posix.Types
-
---import Graphics.Vty
-
---foreign import ccall unsafe "olec_begin"
---    olecBegin_ :: CString -> Ptr CString -> CInt -> IO CInt
-
---foreign import ccall unsafe "olec_debug"
---    olecDebug :: IO ()
-
---foreign import ccall unsafe "gtk_main"
---    gtkMain :: IO ()
-
---data Configuration = Configuration {
---    confFontDescription :: B.ByteString,
---    confColorPalette :: [B.ByteString]
---}
-
---useAsCStrings :: [B.ByteString] -> ([CString] -> IO a) -> IO a
---useAsCStrings rbs f = impl (reverse rbs) [] where
---    impl [] cs = f cs
---    impl (b : bs) cs =
---        B.useAsCString b (\ c -> impl bs (c : cs))
-
---olecBegin :: Configuration -> IO CInt
---olecBegin Configuration {..} =
---    useAsCStrings palette $ \ cs ->
---        withArray cs $ \ palettePtr ->
---            B.useAsCString confFontDescription $ \ fontDescr ->
---                olecBegin_ fontDescr palettePtr 256
---    where
---        palette =
---            if length confColorPalette < 256 then
---                confColorPalette ++ replicate (256 - length confColorPalette) "#ffffff"
---            else
---                take 256 confColorPalette
-
---main :: IO ()
---main = do
---    pts <- olecBegin Configuration {
---        confFontDescription = "Inconsolata 10.5",
---        confColorPalette = [
---            "#1A1A1A", "#D9715F", "#B2CC46", "#FFCB55",
---            "#6486BC", "#AD7FA8", "#06989A", "#D5D5D5"
---        ]
---    }
-
---    --vty <- mkVty mempty {
---    --    inputFd = Just (Fd pts),
---    --    outputFd = Just (Fd pts),
---    --    termName = Just "xterm-256color"
---    --}
-
---    --update vty Picture {
---    --    picCursor = Cursor 10 10,
---    --    picLayers = [text' (withForeColor mempty (ISOColor 2)) "Hello World"],
---    --    picBackground = ClearBackground
---    --}
-
---    --refresh vty
---    --fdWrite (Fd pts) ""
-
---    forkOS gtkMain
-
---    forever $ do
---        olecDebug
---        threadDelay 1000000
-
---    --shutdown vty
-
+import Control.Concurrent
 import Control.Monad.Reader
-import Graphics.UI.Gtk
 
+import Graphics.UI.Gtk
+import Graphics.UI.Gtk.Vte.Vte
+
+import Graphics.Vty
+
+import Foreign.C
+import Foreign.Ptr
+import Foreign.Marshal
+import Foreign.Storable
+
+import System.Posix.Types
+
+foreign import ccall "openpty"
+    openpty :: Ptr CInt -> Ptr CInt -> Ptr () -> Ptr () -> Ptr () -> IO CInt
+
+createPseudoTerminal :: IO (CInt, CInt)
+createPseudoTerminal =
+    with 0 $ \ ptmPtr -> with 0 $ \ ptsPtr -> do
+        r <- openpty ptmPtr ptsPtr nullPtr nullPtr nullPtr
+        when (r /= 0) (error "Could not create pseudo-terminal")
+
+        ptm <- peek ptmPtr
+        pts <- peek ptsPtr
+
+        return (ptm, pts)
 
 isModifierKey :: KeyVal -> Bool
 isModifierKey key =
@@ -97,16 +42,61 @@ main :: IO ()
 main = do
     initGUI
 
+    -- Widgets
     win <- windowNew
+    box <- vBoxNew False 0
+    term <- terminalNew
 
+    -- Events
     on win objectDestroy mainQuit
+
     on win keyPressEvent $ do
         mods <- eventModifier
         key <- eventKeyVal
 
-        unless (isModifierKey key) . liftIO $ do
-            print (mods, key)
+        unless (isModifierKey key) . liftIO $
+            -- Handle key event
+            return ()
+
         return True
 
+    on term childExited $
+        liftIO (putStrLn "Child exited")
+
+    -- Layout
+    containerAdd win box
+    boxPackStart box term PackGrow 0
+
+    -- Configure window
+    windowSetTitle win ("Olec Text Editor" :: String)
+
+    -- Configure terminal
+    terminalSetFontFromString term ("Inconsolata 10.5" :: String)
+
+    (ptm, pts) <- createPseudoTerminal
+    terminalSetPty term (fromIntegral ptm)
+
+    -- Display
     widgetShowAll win
+
+    forkIO $ do
+        vty <- mkVty mempty {
+            inputFd = Nothing,
+            outputFd = Just (Fd pts)
+        }
+
+        update vty Picture {
+            picCursor = NoCursor,
+            picLayers = [text' mempty "Hello World" <|> text' mempty "!"],
+            picBackground = ClearBackground
+        }
+
+        putStrLn "Waiting ..."
+        threadDelay 5000000
+
+        putStrLn "Shutting down ..."
+        shutdown vty
+        mainQuit
+
+    -- Run
     mainGUI
