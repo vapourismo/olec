7b3978e752b0dd5f9a9d573a7456a61cf73a194a
Ole Krüger <ole@vprsm.de>
2015-05-30 01:02:19 +0200

    Use ExistentialQuantification to hide origin type in TFocus

--- src/Control/Concurrent/STM/TFocus.hs
+++ src/Control/Concurrent/STM/TFocus.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE Rank2Types, ExistentialQuantification #-}
 
 module Control.Concurrent.STM.TFocus (
 	TFocus,
@@ -17,37 +17,37 @@ import Control.Monad.STM
 import Control.Concurrent.STM.TVar
 
 -- | Focus on a particular part of a value inside a "TVar"
-data TFocus s t
-	= TFocus (TVar s) (Lens' s t)
+data TFocus s =
+	forall o. TFocus (TVar o) (Lens' o s)
 
 -- | Create a new "TFocus".
-newTFocus :: s -> STM (TFocus s s)
+newTFocus :: s -> STM (TFocus s)
 newTFocus v = toTFocus <$> newTVar v
 
 -- | Create a new "TFocus".
-newTFocusIO :: s -> IO (TFocus s s)
+newTFocusIO :: s -> IO (TFocus s)
 newTFocusIO v = toTFocus <$> newTVarIO v
 
 -- | Lift a "TVar" into a "TFocus".
-toTFocus :: TVar s -> TFocus s s
+toTFocus :: TVar s -> TFocus s
 toTFocus ref = TFocus ref (iso id id)
 
 -- | Create a new "TFocus" which focuses deeper.
-moveTFocus :: TFocus s t -> Lens' t u -> TFocus s u
+moveTFocus :: TFocus t -> Lens' t u -> TFocus u
 moveTFocus (TFocus ref a) b = TFocus ref (a . b)
 
 -- | Retrieve the focused item.
-readTFocus :: TFocus s t -> STM t
+readTFocus :: TFocus t -> STM t
 readTFocus (TFocus ref a) = view a <$> readTVar ref
 
 -- | Retrieve the focused item.
-readTFocusIO :: TFocus s t -> IO t
+readTFocusIO :: TFocus t -> IO t
 readTFocusIO (TFocus ref a) = view a <$> readTVarIO ref
 
 -- | Replace the focused item.
-writeTFocus :: TFocus s t -> t -> STM ()
+writeTFocus :: TFocus t -> t -> STM ()
 writeTFocus (TFocus ref a) v = modifyTVar' ref (set a v)
 
 -- | Modify the focused item.
-modifyTFocus :: TFocus s t -> (t -> t) -> STM ()
+modifyTFocus :: TFocus t -> (t -> t) -> STM ()
 modifyTFocus (TFocus ref a) f = modifyTVar' ref (over a f)
