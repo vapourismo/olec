0da8e4533f3c63865400055d55bad3fafffc2c29
Ole Krüger <ole@vprsm.de>
2015-05-30 15:30:12 +0200

    Expose raw interface file handle

--- src/Olec/Interface.hs
+++ src/Olec/Interface.hs
@@ -1,5 +1,6 @@
 module Olec.Interface (
 	makeInterface,
+	makeRawInterface,
 	module Olec.Interface.Events
 ) where
 
@@ -78,3 +79,43 @@ makeInterface stateReader = do
 				              >>= V.update display . renderPicture mkRenderer
 
 	return (eventChan, updateAction)
+
+-- | Create the main user interface.
+makeRawInterface :: IO (Chan Event, Fd)
+makeRawInterface = do
+	initGUI
+	eventChan <- newChan
+
+	-- Remove style classes
+	mbScreen <- screenGetDefault
+	flip (maybe (return ())) mbScreen $ \ screen -> do
+		cssProvider <- cssProviderNew
+		cssProviderLoadFromString cssProvider "VteTerminal { padding: 6px; }"
+		styleContextAddProviderForScreen screen cssProvider 800
+
+	-- Main window
+	win <- windowNew
+	set win [windowTitle := "Olec Text Editor"]
+
+	-- Box
+	box <- vBoxNew False 0
+	containerAdd win box
+
+	-- Terminal
+	(Fd ptm, pts) <- openPseudoTerminal
+	term <- newTerminal ptm ["#111111"]
+	boxPackStart box term PackGrow 0
+
+	-- Dispatch events
+	forwardKeyPressEvents win eventChan
+	forwardResizeEvents term eventChan
+
+	on win objectDestroy mainQuit
+	on term buttonPressEvent (return True)
+	on term buttonReleaseEvent (return True)
+
+	-- Show interface
+	widgetShowAll win
+	forkOS (finally mainGUI (writeChan eventChan ExitRequest))
+
+	pure (eventChan, pts)
