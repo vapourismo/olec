535dcacbdd204ddd527323023b0d8a7fc015fb8e
Ole Krüger <ole@vprsm.de>
2015-05-30 15:43:02 +0200

    Use raw ByteString to output to Terminal

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,20 @@
 
 module Main where
 
-import Olec.Runtime
+import Control.Concurrent
+
+import Olec.Interface
+
+import System.Posix.IO
+
+import qualified Data.ByteString as B
 
 -- | Entry point
 main :: IO ()
-main =
-	run runtime ()
-	where
-		runtime = pure ()
+main = do
+	(_, outputFd, _) <- makeRawInterface
+	handle <- fdToHandle outputFd
+
+	B.hPut handle "Hello World"
+
+	threadDelay 1000000
