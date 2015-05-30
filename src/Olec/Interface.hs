8b448d14638ac02261e20435a387939e98bbf6ce
Ole Krüger <ole@vprsm.de>
2015-05-30 18:18:17 +0200

    Add functions to interact with a Handle to a pseudo-terminal

--- src/Olec/Interface.hs
+++ src/Olec/Interface.hs
@@ -1,12 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Olec.Interface (
+	-- * Initializers
 	makeInterface,
 	makeRawInterface,
-	module Olec.Interface.Events
+
+	-- * Interaction
+	writeCursorPosition,
+	writeForegroundColor,
+	writeBackgroundColor,
+	writeText,
+	writeString,
+
+	-- * Re-exports
+	module Olec.Interface.Events,
 ) where
 
 import Control.Monad
 import Control.Exception
 import Control.Concurrent
+
+import Data.Word
+
+import qualified Data.ByteString as B
+
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
 import qualified Graphics.Vty as V
 
@@ -14,6 +33,9 @@ import Graphics.UI.Gtk hiding (Size)
 import Graphics.UI.Gtk.General.StyleContext
 import Graphics.UI.Gtk.General.CssProvider
 
+import System.IO
+
+import System.Posix.IO
 import System.Posix.Types
 import System.Posix.Terminal
 
@@ -31,12 +53,12 @@ launchUI eventChan = do
 	mbScreen <- screenGetDefault
 	flip (maybe (return ())) mbScreen $ \ screen -> do
 		cssProvider <- cssProviderNew
-		cssProviderLoadFromString cssProvider "VteTerminal { padding: 6px; }"
+		cssProviderLoadFromString cssProvider ("VteTerminal { padding: 6px; }" :: T.Text)
 		styleContextAddProviderForScreen screen cssProvider 800
 
 	-- Main window
 	win <- windowNew
-	set win [windowTitle := "Olec Text Editor"]
+	set win [windowTitle := ("Olec Text Editor" :: T.Text)]
 
 	-- Box
 	box <- vBoxNew False 0
@@ -88,8 +110,50 @@ makeInterface stateReader = do
 	return (eventChan, updateAction)
 
 -- | Create the main user interface.
-makeRawInterface :: IO (Chan Event, Fd, IO Size)
+makeRawInterface :: IO (Chan Event, Handle, IO Size)
 makeRawInterface = do
 	eventChan <- newChan
 	(pts, sizeAction) <- launchUI eventChan
-	pure (eventChan, pts, sizeAction)
+	output <- fdToHandle pts
+
+	pure (eventChan, output, sizeAction)
+
+-- | Set the cursor position.
+writeCursorPosition :: Handle -> Int -> Int -> IO ()
+writeCursorPosition h x y = do
+	B.hPut h "\ESC["
+	hPutStr h (show (y + 1))
+	B.hPut h ";"
+	hPutStr h (show (x + 1))
+	B.hPut h "H"
+
+-- | Write a RGB triple.
+writeRGB :: Handle -> Word8 -> Word8 -> Word8 -> IO ()
+writeRGB h r g b = do
+	hPutStr h (show r)
+	B.hPut h ";"
+	hPutStr h (show g)
+	B.hPut h ";"
+	hPutStr h (show b)
+
+-- | Set the foreground color.
+writeForegroundColor :: Handle -> Word8 -> Word8 -> Word8 -> IO ()
+writeForegroundColor h r g b = do
+	B.hPut h "\ESC[38;2;"
+	writeRGB h r g b
+	B.hPut h "m"
+
+-- | Set the background color.
+writeBackgroundColor :: Handle -> Word8 -> Word8 -> Word8 -> IO ()
+writeBackgroundColor h r g b = do
+	B.hPut h "\ESC[48;2;"
+	writeRGB h r g b
+	B.hPut h "m"
+
+-- | Write Text.
+writeText :: Handle -> T.Text -> IO ()
+writeText h t = B.hPut h (T.encodeUtf8 t)
+
+-- | Write String.
+writeString :: Handle -> String -> IO ()
+writeString = hPutStr
