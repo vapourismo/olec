6540135896f0dd638394e62eb3f5b367b6d6ed3d
Ole Krüger <ole@vprsm.de>
2015-06-03 00:49:27 +0200

    I might have overdone it

--- src/Main.hs
+++ src/Main.hs
@@ -1,91 +1,120 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Main where
 
-import Control.Concurrent
 import Control.Monad.Reader
+import Control.Monad.State.Strict
 
-import Data.Time
 import Data.IORef
-import Data.Metrics
-
-import System.Random
+import Data.Tuple
 
 import Olec.Interface
 
--- |
-data Rainbow = Rainbow
+-- | Component
+class Component r s where
+	layoutRT :: Runtime r s Layout ()
 
-instance Visual Rainbow where
-	visualize _ = do
-		(width, height) <- getSize
-		forM_ [0 .. height - 1] $ \ y -> do
-			moveCursor 0 y
-			forM_ [0 .. width - 1] $ \ _ -> do
-				fg <- liftIO (Color <$> randomIO <*> randomIO <*> randomIO)
-				bg <- liftIO (Color <$> randomIO <*> randomIO <*> randomIO)
+	paintRT :: Runtime r s IO ()
 
-				setForegroundColor fg
-				setBackgroundColor bg
+-- | Component which may be used by the root UI facilities
+class (Component r s) => RootComponent r s where
+	startRT :: Runtime r s IO ()
 
-				c <- liftIO (randomRIO ('A', 'Z'))
-				drawString [c]
+	exitRT :: Runtime r s IO ()
 
--- |
-data Clock = Clock (IORef UTCTime)
+	inputRT :: KeyEvent -> Runtime r s IO Bool
 
-instance Visual Clock where
-	visualize (Clock ref) = do
-		tm <- liftIO (readIORef ref)
-		let text = formatTime defaultTimeLocale "%T" tm
+-- | Handke to a Component
+data Handle r s = Handle {
+	hStatic   :: r,
+	hDynamic  :: IORef s,
+	hDelegate :: LayoutDelegate
+}
 
-		(w, _) <- getSize
-		let [(l, _), (m, _), (r, _)] =
-			divideMetric [LeftOver () :: DivisionHint Int Float (),
-			              Absolute (stringWidth text) (),
-			              LeftOver ()] w
+instance (Component r s) => Widget (Handle r s) where
+	layout h = withComponent h layoutRT
 
-		drawString (replicate l ' ')
-		drawString (fitString m text)
-		drawString (replicate r ' ')
+	paint h = withComponent h paintRT
 
--- |
-newClock :: Display -> IO (FlatWidget Clock)
-newClock d = do
-	clockState <- newIORef =<< getCurrentTime
-	clock <- newFlatWidget d (Clock clockState)
+instance (RootComponent r s) => RootWidget (Handle r s) where
+	data Setup (Handle r s) = ConstructRootWidget (Construct (r, s))
 
-	forkIO $ forever $ do
-		threadDelay 1000000
-		tm <- getCurrentTime
-		writeIORef clockState tm
-		paint clock
+	setup d (ConstructRootWidget c) = do
+		h <- runReaderT (c >>= uncurry newHandle) d
+		withComponent h startRT
+		pure h
 
-	pure clock
+	exit h = withComponent h exitRT
 
--- |
-data MyRootWidget = MyRootWidget (FlatWidget Rainbow) (FlatWidget Clock)
+	input h e = withComponent h (inputRT e)
 
-instance Widget MyRootWidget where
-	layout (MyRootWidget rb clock) =
-		divideVert [LeftOver (layout rb), Absolute 1 (layout clock)]
+-- | Component Runtime
+newtype Runtime r s m a = Runtime { runM :: ReaderT (Handle r s) m a }
+	deriving (Functor, Applicative, Monad, MonadTrans, MonadIO)
 
-	paint (MyRootWidget rb clock) = do
-		paint rb
-		paint clock
+instance (Monad m) => MonadReader r (Runtime r s m) where
+	ask = Runtime (reader hStatic)
 
-instance RootWidget MyRootWidget where
-	data Setup MyRootWidget = SetupMyRootWidget
+	local f (Runtime r) = Runtime (local (\ h -> h {hStatic = f (hStatic h)}) r)
 
-	input _ _ = do
-		exitUI
-		pure False
+instance (MonadIO m) => MonadState s (Runtime r s m) where
+	get = Runtime (reader hDynamic >>= liftIO . readIORef)
 
-	exit _ = putStrLn "Bye"
+	put x = Runtime (reader hDynamic >>= liftIO . flip writeIORef x)
 
-	setup d _ = MyRootWidget <$> newFlatWidget d Rainbow <*> newClock d
+	state f = Runtime (reader hDynamic >>= liftIO . flip atomicModifyIORef' (swap . f))
+
+-- | Do something with a Component.
+withComponent :: Handle r s -> Runtime r s m a -> m a
+withComponent h (Runtime r) = runReaderT r h
+
+-- | ???
+shareRT :: (Monad n) => Runtime r s m a -> Runtime r s n (m a)
+shareRT m = flip withComponent m <$> Runtime ask
+
+-- | Render something.
+renderRT :: (MonadIO m) => Renderer a -> Runtime r s m a
+renderRT r = Runtime ask >>= liftIO . runRenderer r . hDelegate
+
+-- | Use this when constructing Components.
+type Construct = ReaderT Display IO
+
+-- | Construct a Component and return its Handle.
+newHandle :: r -> s -> Construct (Handle r s)
+newHandle r s =
+	Handle r <$> liftIO (newIORef s)
+	         <*> (ask >>= liftIO . toLayoutDelegate)
+
+data ClockR = ClockR
+
+data ClockS = ClockS Word Word
+
+instance Component ClockR ClockS where
+	layoutRT = pure ()
+
+	paintRT = do
+		ClockS n r <- get
+		renderRT (drawString (show (n, r)))
+		put (ClockS n (r + 1))
+
+instance RootComponent ClockR ClockS where
+	startRT =
+		liftIO (putStrLn "Handleello")
+
+	exitRT =
+		liftIO (putStrLn "Good bye")
+
+	inputRT e = do
+		ClockS n r <- get
+		liftIO (print e)
+		put (ClockS (n + 1) r)
+		paintRT
+		pure True
 
 -- | Entry point
 main :: IO ()
-main = launchUI SetupMyRootWidget
+main = launchUI (ConstructRootWidget (pure (ClockR, ClockS 0 0)))
