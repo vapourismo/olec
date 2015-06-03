f3c1014fb61cc9ece19af5da05cc7b34f09b7c94
Ole Krüger <ole@vprsm.de>
2015-06-04 00:49:12 +0200

    Widget system draft

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,10 @@
 
 module Main where
 
-import Control.Concurrent
-
+import Data.IORef
 import Data.Metrics
+
+import qualified Data.Text as T
 
 import Olec.Interface.GTK
 import Olec.Interface.Types
@@ -19,13 +20,62 @@ fillArea style c (w, h) =
 		line = replicate w c
 		hints = replicate h (Absolute 1 (drawString style line))
 
+newtype LayoutDelegate = LayoutDelegate (IORef (Position, Size))
+
+-- |
+newLayoutDelegate :: IO LayoutDelegate
+newLayoutDelegate = LayoutDelegate <$> newIORef ((0, 0), (0, 0))
+
+class Visual a where
+	visualize :: a -> Visualiser
+
+class Layout a where
+	performLayout :: a -> Position -> Size -> IO ()
+
+class (Layout a) => Widget a where
+	paintWidget :: (Output o) => a -> o -> IO ()
+
+instance Layout LayoutDelegate where
+	performLayout (LayoutDelegate ref) origin size =
+		writeIORef ref (origin, size)
+
+data Handle a = Handle a LayoutDelegate
+
+instance Layout (Handle a) where
+	performLayout (Handle _ ld) = performLayout ld
+
+instance (Visual a) => Widget (Handle a) where
+	paintWidget (Handle w (LayoutDelegate ref)) out = do
+		(origin, size) <- readIORef ref
+		outputImage out origin (visualize w size)
+
+-- |
+newHandle :: a -> IO (Handle a)
+newHandle w = Handle w <$> newLayoutDelegate
+
+-- |
+bindWidget :: (EventSource o, Output o, Widget a) => o -> a -> IO ()
+bindWidget o w = do
+	onResize o $ \ size -> do
+		clearOutput o
+		performLayout w (0, 0) size
+		paintWidget w o
+
+	size <- sizeOfOutput o
+
+	clearOutput o
+	performLayout w (0, 0) size
+	paintWidget w o
+
+instance Visual T.Text where
+	visualize = drawText (Style (Color 255 255 255) (Color 0 0 0))
+
 main :: IO ()
 main = do
-	d <- newInterface
+	o <- newInterface
+	onKeyEvent o (\ _ -> True <$ exitInterface)
 
-	onKeyEvent d (\ _ -> True <$ exitInterface)
-
-	clearOutput d
-	outputImage d (0, 0) (fillArea (Style (Color 255 0 0) (Color 255 255 255)) 'x' (10, 10))
+	h <- newHandle ("Hello World" :: T.Text)
+	bindWidget o h
 
 	runInterface
