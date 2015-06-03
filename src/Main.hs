46de93e95a924ca31eace44e318ed763abd40f52
Ole Krüger <ole@vprsm.de>
2015-06-03 21:59:22 +0200

    Fill Area

--- src/Main.hs
+++ src/Main.hs
@@ -4,12 +4,19 @@ module Main where
 
 import Control.Concurrent
 
+import qualified Data.Text as T
+
 import Olec.Interface.GTK
 import Olec.Visual
+
+-- |
+fillArea :: Style -> Char -> Visualiser
+fillArea style c (w, h) =
+	VAlign (replicate h (Text style (T.pack (replicate w c))))
 
 main :: IO ()
 main = do
 	d <- newInterface
 	size <- dimensions d
-	display d (0, 0) (drawText (Style (Color 255 0 0) (Color 255 255 255)) "Hello World" size)
+	display d (0, 0) (fillArea (Style (Color 255 0 0) (Color 255 255 255)) 'x' (10, 10))
 	threadDelay 5000000
