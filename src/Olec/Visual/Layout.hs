baec49ff49b0fc09ba5823ba252f23aec866276b
Ole Krüger <ole@vprsm.de>
2015-06-08 19:38:11 +0200

    Update Layout impl

--- src/Olec/Visual/Layout.hs
+++ src/Olec/Visual/Layout.hs
@@ -1,8 +1,35 @@
 module Olec.Visual.Layout (
-	Layout (..),
+	Layout,
+	runLayout,
+	LayoutElement (..),
+	vlayout
 ) where
+
+import Control.Monad.Reader
+
+import Data.Metrics
 
 import Olec.Visual.Types
 
-class Layout a where
-	updateLayout :: a -> Position -> Size -> IO ()
+-- | Layout
+type Layout = ReaderT (Position, Size) IO ()
+
+-- | Execute layout actions
+runLayout :: Position -> Size -> Layout -> IO ()
+runLayout o s l = runReaderT l (o, s)
+
+-- | Type which have an inner Layout structure
+class LayoutElement a where
+	layout :: a -> Layout
+
+-- | Align elements vertically.
+vlayout :: [DivisionHint Int Float Layout] -> Layout
+vlayout hints = do
+	((_, y), (_, h)) <- ask
+	make y (divideMetric hints h)
+	where
+		make :: Int -> [(Int, Layout)] -> ReaderT (Position, Size) IO ()
+		make _ [] = pure ()
+		make y ((h, l) : ls) = do
+			local (\ ((x, _), (w, _)) -> ((x, y), (w, h))) l
+			make (y + h) ls
--- src/Olec/Visual/Slot.hs
+++ src/Olec/Visual/Slot.hs
@@ -4,11 +4,11 @@ module Olec.Visual.Slot (
 	-- * Slot
 	Slot,
 	toSlot,
-	updateSlot,
 	paintSlot,
 ) where
 
 import Control.Monad
+import Control.Monad.Reader
 
 import Data.IORef
 
@@ -18,19 +18,15 @@ import Olec.Visual.Layout
 -- | Layout Slot
 data Slot = forall o. (Canvas o) => Slot o (IORef (Position, Size))
 
-instance Layout Slot where
-	updateLayout = updateSlot
+instance LayoutElement Slot where
+	layout (Slot _ ref) =
+		ask >>= liftIO . writeIORef ref
 
 -- | Create a new "Slot". The slot initially occupies the entire canvas.
 toSlot :: (Canvas o) => o -> IO Slot
 toSlot out = do
 	size <- sizeOfCanvas out
 	Slot out <$> newIORef ((0, 0), size)
-
--- | Update the "Slot" with new bounds.
-updateSlot :: Slot -> Position -> Size -> IO ()
-updateSlot (Slot _ ref) origin size =
-	writeIORef ref (origin, size)
 
 -- | Paint something in the designated area associated with the "Slot".
 paintSlot :: Slot -> Painter -> IO ()
