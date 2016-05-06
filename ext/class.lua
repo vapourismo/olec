da495e1d8f5539d9565da6926ecf7276b5ad37a0
Ole <ole@vprsm.de>
2016-05-06 17:28:06 +0200

    Add Lua class module

---
+++ ext/class.lua
@@ -0,0 +1,27 @@
+return function (name)
+	local tpl = {
+		__init = function () end,
+		__name = name,
+		__tostring = function (self)
+			local ret = (self.__name or name or "UnnamedClass") .. " {"
+			local sep = ""
+
+			for k, v in pairs(self) do
+				ret = ret .. sep .. tostring(k) .. " = " .. tostring(v)
+				sep = ", "
+			end
+
+			return ret .. "}"
+		end
+	}
+
+	tpl.__index = tpl
+
+	return setmetatable(tpl, {
+		__call = function (self, ...)
+			local instance = setmetatable({}, tpl)
+			instance:__init(...)
+			return instance
+		end
+	})
+end
