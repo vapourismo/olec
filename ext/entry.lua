2349f88766e479509da933f06ca68e543c993552
Ole <ole@vprsm.de>
2016-05-07 20:19:27 +0200

    Add Clip helper class

--- ext/entry.lua
+++ ext/entry.lua
@@ -1,15 +1,62 @@
-TermBox.init()
+require("ext/class")
 
-local str1 = "He两llo World"
-local str2 = "Hello World"
+local Clip = class()
 
-TermBox.changeCells(0, 0, str1, TermBox.Red, TermBox.Default)
-TermBox.changeCells(0, 1, str2, TermBox.Red, TermBox.Default)
+function Clip:__init(x, y, width, height)
+	self.x = x
+	self.y = y
+	self.width = width
+	self.height = height
+end
 
-TermBox.changeCells(str1:width(), 0, "!", TermBox.Red, TermBox.Default)
-TermBox.changeCells(str2:width(), 1, "!", TermBox.Red, TermBox.Default)
+function Clip:changeCells(x, y, contents, fg, bg)
+	-- Abort if the starting position is not inside the clip
+	if x < 0 or y < 0 or y >= self.height or x >= self.width then
+		return
+	end
 
-TermBox.present()
+	local width = Util.stringWidth(contents)
 
-sleep(5)
-TermBox.shutdown()
+	-- Cut off overlapping contents
+	if x + width > self.width then
+		local maxWidth = self.width - x
+		local curWidth = 0
+		local codepoints = {}
+
+		-- Iterate over all codepoints
+		for _, ch in utf8.codes(contents) do
+			local charWidth = Util.wcharWidth(ch)
+
+			-- Exit loop if adding the current code point would make the result string wider than
+			-- the given maximum width
+			if curWidth + charWidth > maxWidth then
+				break
+			end
+
+			table.insert(codepoints, ch)
+			curWidth = curWidth + charWidth
+		end
+
+		-- Combine extracted codepoints into a string
+		contents = utf8.char(table.unpack(codepoints))
+	end
+
+	-- Do the actual changing
+	TermBox.changeCells(self.x + x, self.y + y, contents, fg, bg)
+end
+
+function Clip:fill(fg, bg, ch)
+	fg = fg or TermBox.Default
+	bg = bg or TermBox.Default
+	ch = ch or 32
+
+	for x = 0, self.width - 1, 1 do
+		for y = 0, self.height - 1, 1 do
+			TermBox.changeCell(self.x + x, self.y + y, ch, fg, bg)
+		end
+	end
+end
+
+print("Hello")
+Util.sleep(2)
+print("World")
